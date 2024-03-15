d72cd84a8d43aa061d2865cc03bfff05b26b83eb
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-03-15 17:58:48 +0800

    fix: 在 windows 系统 active_indicators 失效

--- lua/harpoon-lualine/init.lua
+++ lua/harpoon-lualine/init.lua
@@ -4,29 +4,34 @@ local harpoon = utils.lazy_require("harpoon")
 local M = {}
 
 M.status = function(options)
-  local list = harpoon:list()
-  local root_dir = list.config:get_root_dir()
-  local current_file_path = vim.api.nvim_buf_get_name(0)
+	local list = harpoon:list()
+	local root_dir = list.config:get_root_dir()
+	local current_file_path = vim.api.nvim_buf_get_name(0)
 
-  local length = list:length()
+	local length = list:length()
 
-  local status = ""
+	local status = ""
 
-  for i = 1, length do
-    local value = list:get(i).value
-    local full_path = root_dir .. "/" .. value
-    if full_path == current_file_path then
-      status = status .. " " .. options.active_indicators[i]
-    else
-      status = status .. " " .. options.indicators[i]
-    end
-  end
+	for i = 1, length do
+		local value = list:get(i).value
+		local full_path
+		if vim.loop.os_uname().sysname == "Windows_NT" then
+			full_path = root_dir .. "\\" .. value
+		else
+			full_path = root_dir .. "/" .. value
+		end
+		if full_path == current_file_path then
+			status = status .. " " .. options.active_indicators[i]
+		else
+			status = status .. " " .. options.indicators[i]
+		end
+	end
 
-  return status
+	return status
 end
 
 M.setup = function(_)
-  -- do nothing, just for compatibility
+	-- do nothing, just for compatibility
 end
 
 return M
